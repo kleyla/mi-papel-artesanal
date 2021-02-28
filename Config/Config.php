a41e9296054bffbcc60b7fcf2293f8363d562443
kleyla <kleyla01@live.com>
2021-02-28 17:51:39 -0400

    starting

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+// define("BASE_URL", "http://localhost/mvc-tienda/");
+const BASE_URL = "http://localhost/mi-papel-artesanal/";
+const LIBS = "Libraries/";
+const VIEWS = "Views/";
+
+// zona horaria
+date_default_timezone_set("America/La_Paz");
