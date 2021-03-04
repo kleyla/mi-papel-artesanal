817d0a9d850c474cd7506db06c4b77545c6b71ff
kleyla <kleyla01@live.com>
2021-03-03 20:02:27 -0400

    title changed

--- Views/template/header.php
+++ Views/template/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Penguin game</title>
+    <title>Mi papel artesanal</title>
     <link rel="shortcut icon" href="<?= media(); ?>imgs/papel.svg" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link rel="stylesheet" href="<?= media(); ?>css/styles.css">
